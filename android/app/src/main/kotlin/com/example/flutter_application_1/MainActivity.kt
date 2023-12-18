f550201138476bc960142720fdac20e3492f4bd4
billclinton <bill.cinton2804@gmail.com>
2023-12-18 20:35:49 +0700

    update

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_1
+package com.example.uas_bill_clinton_pemrograman_mobile
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
+import 'package:uas_bill_clinton_pemrograman_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
